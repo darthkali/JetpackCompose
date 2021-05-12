bfddd66e33be4f4d9081e7ad9d10f0ed7dac3329
darthkali <danny.steinbrecher@fh-erfurt.de>
2021-05-12 14:38:00 +0200

    02

--- 02_CoreComposeApplication/app/src/main/java/com/example/corecomposeapplication/MainActivity.kt
+++ 02_CoreComposeApplication/app/src/main/java/com/example/corecomposeapplication/MainActivity.kt
@@ -3,15 +3,16 @@ package com.example.corecomposeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.corecomposeapplication.ui.theme.CoreComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,22 +30,36 @@ fun MainScreen() {
         color = Color.DarkGray,
         modifier = Modifier.fillMaxSize()
     ) {
-        Surface(
-            color = Color.Magenta,
-            modifier = Modifier.wrapContentSize(),
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(
-                text = "Wrapped Content",
-                style = MaterialTheme.typography.h3
-            )
+            HorizontalColorBar(Color.Red)
+            HorizontalColorBar(Color.Magenta)
+            HorizontalColorBar(Color.Cyan)
+            HorizontalColorBar(Color.Yellow)
+            HorizontalColorBar(Color.Blue)
         }
+
     }
 }
+
+@Composable
+fun HorizontalColorBar(color: Color) {
+    Surface(
+        color = color,
+        modifier = Modifier
+            .height(600.dp)
+            .width(60.dp)
+    ) {}
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    CoreComposeApplicationTheme {
-        MainScreen()
-    }
+
+    MainScreen()
+
 }
