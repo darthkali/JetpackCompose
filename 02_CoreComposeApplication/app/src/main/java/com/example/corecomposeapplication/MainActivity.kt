17625988dd6b9131f1523e5cff30ca4ea2d5ad7c
darthkali <danny.steinbrecher@fh-erfurt.de>
2021-05-12 14:56:26 +0200

    rows and and columns

--- 02_CoreComposeApplication/app/src/main/java/com/example/corecomposeapplication/MainActivity.kt
+++ 02_CoreComposeApplication/app/src/main/java/com/example/corecomposeapplication/MainActivity.kt
@@ -4,16 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.corecomposeapplication.ui.theme.CoreComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,27 +27,32 @@ fun MainScreen() {
         color = Color.DarkGray,
         modifier = Modifier.fillMaxSize()
     ) {
-        Row(
+        Column(
             modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            HorizontalColorBar(Color.Red)
-            HorizontalColorBar(Color.Magenta)
-            HorizontalColorBar(Color.Cyan)
-            HorizontalColorBar(Color.Yellow)
-            HorizontalColorBar(Color.Blue)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                ColoredSquare(Color.Red)
+                ColoredSquare(Color.Magenta)
+            }
+            ColoredSquare(Color.Cyan)
+            ColoredSquare(Color.Yellow)
+            ColoredSquare(Color.Blue)
         }
 
     }
 }
 
 @Composable
-fun HorizontalColorBar(color: Color) {
+fun ColoredSquare(color: Color) {
     Surface(
         color = color,
         modifier = Modifier
-            .height(600.dp)
+            .height(60.dp)
             .width(60.dp)
     ) {}
 }
