0f785d737596e095668705fcde9c313b5361d9ec
darthkali <danny.steinbrecher@fh-erfurt.de>
2021-05-13 10:54:07 +0200

    added a List

---
+++ 03_DynamicContentExample/app/src/main/java/com/example/dynamiccontentexample/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.dynamiccontentexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dynamiccontentexample.ui.theme.DynamicContentExampleTheme
+
+val namesList: ArrayList<String> = arrayListOf("John", "Michael", "Danny", "Jana", "Manuela")
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GreetingList(namesList)
+        }
+    }
+}
+
+@Composable
+fun GreetingList(names: List<String>) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        for (name in names) {
+            Greeting(name)
+        }
+
+        Button(onClick = { namesList.add("New Name") }) {
+            Text("add new name")
+        }
+    }
+
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello $name!",
+        style = MaterialTheme.typography.h4
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GreetingList(namesList)
+}
